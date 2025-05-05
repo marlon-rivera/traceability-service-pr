2b41326bfc8ee6183118d50c4f20d02041a12e81
marlon-rivera <marlon.rivera@uptc.edu.co>
2025-05-04 23:28:16 -0500

    Project structure

---
+++ src/test/java/com/pragma/traceability_order_service/TraceabilityOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.traceability_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceabilityOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
